73ef38913c70ec2a5a08da7823098d3175a1dd55
Jue Hou <jue.hou@moffett.ai>
2024-05-10 13:55:38 +0800

    GEN-268: Add Rvv address mapping

--- hw_config.h
+++ hw_config.h
@@ -185,6 +185,12 @@ const size_t kNumVpuVectorReg = 4;
 const size_t kVpuVectorRegDepth = 64;
 const size_t kNumVpuScalarReg = 32;
 
+// RVV address mapping
+const size_t kCoreRvvSysStartAddr = 0x000040000000; const size_t kCoreRvvSysEndAddr   = 0x00005FFFFFFF;  
+const size_t kCoreRvvDdrStartAddr = 0x000080000000; const size_t kCoreRvvDdrEndAddr   = 0x00607FFFFFFF;
+const size_t kPeRvvSysStartAddr   = 0x000040000000; const size_t kPeRvvSysEndAddr     = 0x00005FFFFFFF;
+const size_t kPeRvvDdrStartAddr   = 0x000080000000; const size_t kPeRvvDdrEndAddr     = 0x00008000FFFF;
+
 // Chip-Level address mapping
 const size_t kCore0StartAddr      = 0x40000000;     const size_t kCore0EndAddr        = 0x4FFFFFFF;
 const size_t kCore1StartAddr      = 0x50000000;     const size_t kCore1EndAddr        = 0x5FFFFFFF;
